a63fa98e2796e4dbcd99a7f4b6d6ec307840d694
TenochMontes <>
2021-10-26 18:52:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+* @author Tenoch
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+
+/**
+* @param a primer entero de la ecuacion
+* @param b segundo entero de la ecuacion
+* @return la suma de los enteros de la ecuacion
+*/
+public static int suma(int a, int b){
+  return a+b;
+}
+}
---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Main","l":"suma(int, int)","url":"suma(int,int)"}]
